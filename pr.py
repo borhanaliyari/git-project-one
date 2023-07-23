5b6fdd5f4c2193d9f2d687082d24129630a92ec0
borhan <borhanaliyari@gmail.com>
2023-07-23 22:51:10 +0330

    update  label with button and make html file

--- pr.py
+++ pr.py
@@ -4,7 +4,7 @@ import random
 class Rand(Tk):
     def __init__(self) :
         super().__init__()
-        self.title("Random Game")
+        self.title("Random Game for children")
         self.minsize(640,520)
         self.configure(background="gray")
         self.ui()
@@ -14,14 +14,14 @@ class Rand(Tk):
         self.label.pack()
         but = Button(text = "click",background="red",font=("Arial",12),command=self.button)
         but.pack()
-        self.label1 = Label(text = "",background="pink",font=("Arial",30))
+        self.label1 = Label(text = "",background="green",font=("Arial",30))
         self.label1.pack()
         label2 = Label(text = "do you love it",background="red",font=("Arial",50),foreground="yellow")
         label2.pack()
 
 
     def button(self):
-        self.label1.config(text="salamchalem",padx=25)
+        self.label1.config(text="salamchalem",padx=25,pady=15)
             
         
 
