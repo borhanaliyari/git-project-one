73613b42d9e50ac7741d1fbfeba686df874db605
borhan <borhanaliyari@gmail.com>
2023-07-23 21:36:10 +0330

    change to number

--- pr.py
+++ pr.py
@@ -14,10 +14,17 @@ class Rand(Tk):
         self.label.pack()
         but = Button(text = "click",background="red",font=("Arial",12),command=self.button)
         but.pack()
+        self.label1 = Label(text = "",background="pink",font=("Arial",30))
+        self.label1.pack()
 
 
     def button(self):
-        self.label.config(text="Thanks for choosing us" )
+        num = "09"
+        rand_num = random.randint(0,20)
+        for i in num:
+            num += str(rand_num)
+        self.label1.config(text=num)
+            
         
 
 
